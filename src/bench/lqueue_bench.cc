c1b9967c02742680a46c175e88aeb332da57d958
Joel Meuleman <j-jolabola@live.com>
2025-07-18 15:08:15 -0700

    LQueue PRoducer consumer benchmark.

--- src/bench/lqueue_bench.cc
+++ src/bench/lqueue_bench.cc
@@ -1,37 +1,70 @@
 #include "../lqueue.hh"
+#include <atomic>
 #include <benchmark/benchmark.h>
+#include <thread>
 
 const size_t kQueueSize = 2048;
 
 using QueueType = spsc::LQueue<int, kQueueSize>;
 
-void produce(QueueType &queue) {
-  for (int i = 0; i < kQueueSize; ++i) {
-    queue.push(i);
-  }
-};
-
-void consume(QueueType &queue) {
-  for (int i = 0; i < kQueueSize; ++i) {
-    queue.pop();
-  }
-};
-
 static void BM_LQueuePush(benchmark::State &state) {
   spsc::LQueue<int, kQueueSize> queue;
   for (auto _ : state) {
-    produce(queue);
+    for (int i = 0; i < kQueueSize; ++i) {
+      queue.push(i);
+    }
   }
 }
 BENCHMARK(BM_LQueuePush);
 
 static void BM_LQueuePop(benchmark::State &state) {
   spsc::LQueue<int, kQueueSize> queue;
-  produce(queue);
+  for (int i = 0; i < kQueueSize; ++i) {
+    queue.push(i);
+  }
   for (auto _ : state) {
-    consume(queue);
+    for (int i = 0; i < kQueueSize; ++i) {
+      queue.pop();
+    }
   }
 }
 BENCHMARK(BM_LQueuePop);
+
+static void BM_LQueueProduceConsume(benchmark::State &state) {
+  for (auto _ : state) {
+    spsc::LQueue<int, kQueueSize> queue;
+    std::atomic<bool> start_flag{false};
+
+    auto producer = [&queue, &start_flag]() {
+      while (!start_flag.load(std::memory_order_acquire)) {
+        std::this_thread::yield();
+      }
+      for (int i = 0; i < kQueueSize; ++i) {
+        queue.push(i);
+      }
+    };
+
+    auto consumer = [&queue, &start_flag]() {
+      size_t items_popped = 0;
+      while (!start_flag.load(std::memory_order_acquire)) {
+        std::this_thread::yield();
+      }
+      while (items_popped < kQueueSize) {
+        if (queue.pop()) {
+          ++items_popped;
+        }
+      }
+    };
+
+    std::thread producer_thread(producer);
+    std::thread consumer_thread(consumer);
+
+    start_flag.store(true, std::memory_order_release);
+
+    producer_thread.join();
+    consumer_thread.join();
+  }
+}
+BENCHMARK(BM_LQueueProduceConsume);
 
 BENCHMARK_MAIN();
